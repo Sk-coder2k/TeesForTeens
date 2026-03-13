94204b845e37eb7ff2ea043dbc605128e3443374
Sasi Kumar <teesforteens.support@gmail.com>
2026-03-13 20:07:17 +0530

    Fix: cancelledOrders defined before monthly loop

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -249,12 +249,6 @@ export const getDashboardStats = async (req, res) => {
       ([month, data]) => ({ month, ...data }),
     );
 
-    const cancelledOrders = orders.filter((o) => o.orderStatus === "Cancelled");
-    const cancelledRevenue = cancelledOrders.reduce(
-      (sum, o) => sum + (o.totalPrice || 0),
-      0,
-    );
-
     res.json({
       totalRevenue,
       totalOrders: activeOrders.length,
